71b03cca5ccea6fe4027777d7922d836ade116d8
LordUzandre <engstrom.jocke@gmail.com>
2023-10-20 16:02:36 +0200

    - (still functioning)

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -7,10 +7,19 @@ public class CameraManager : MonoBehaviour
 {
     [HideInInspector] public CinemachineVirtualCamera mainCamera;
     private Transform player;
+    public float distanceToPlayer;
 
     private void Start()
     {
         mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CinemachineVirtualCamera>();
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        distanceToPlayer = Vector3.Distance(this.transform.position, player.position);
+    }
+
+    void FixedUpdate()
+    {
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out RaycastHit hit, distanceToPlayer))
+        {
+        }
     }
 }
