ab1d9194e3fdac540368187dd79f412e20856df8
LordUzandre <engstrom.jocke@gmail.com>
2023-11-13 15:29:57 +0100

    Added Node Editor

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Dialogue", menuName = "New Dialogue")]
+[CreateAssetMenu(fileName = "Dialogue", menuName = "Dialogue/New Dialogue")]
 public class Dialogue : ScriptableObject
 {
     //Maximum number of lines per sentence
