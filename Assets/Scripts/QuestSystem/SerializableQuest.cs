af1025f8f8fff57f6715d711f27a0094ed39315a
LordUzandre <59932652+LordUzandre@users.noreply.github.com>
2024-04-20 21:43:38 +0200

    -

--- Assets/Scripts/QuestSystem/SerializableQuest.cs
+++ Assets/Scripts/QuestSystem/SerializableQuest.cs
@@ -10,21 +10,42 @@ namespace Arcy.Quests
 		[SerializeField] int questGUID = 0;
 		[Header("UI")]
 		[SerializeField] string QuestName;
+		[SerializeField] string questGiver;
 		[Header("Objectives")]
 		[SerializeField] QuestObjectiveEnum questState;
 		[SerializeField] QuestObjective[] objectives;
 		[SerializeField] int objectiveIndex = 0;
-		[Header("Requirements before quest")]
+		[Header("Requirements")]
+		[SerializeField] Utils.SerializableDictionary<string, int> requirements = new Utils.SerializableDictionary<string, int>();
+		[Space]
+		[SerializeField] string requiredTeamMember = "";
 		[SerializeField] int requiredPlayerLvl = 0;
 		[SerializeField] int requiredPreviousQuestID = 0;
 		[SerializeField] int requiredItemID = 0;
 		[SerializeField] int requiredPreviousDialogueID = 0;
-		[SerializeField] int requiredDialogueID = 0;
+		[SerializeField] int initialDialogueID = 0;
 		[SerializeField] int requiredBattleID = 0;
 		[Header("Rewards")]
 		Inventory.InventorySlot[] rewards;
 
-		private void OnValidate() { if (questGUID == 0) questGUID = Utils.GuidGenerator.guid(); }
+		private void Reset()
+		{
+			if (requirements.Count == 0)
+			{
+				requirements.Add("requiredPlayerLvl", 0);
+				requirements.Add("requiredTeamMember", 0);
+				requirements.Add("requiredPreviousQuestID", 0);
+				requirements.Add("requiredItemID", 0);
+				requirements.Add("requiredPreviousDialogueID", 0);
+				requirements.Add("initialDialogueID", 0);
+				requirements.Add("requiredBattleID", 0);
+			}
+		}
+
+		private void OnValidate()
+		{
+			if (questGUID == 0) questGUID = Utils.GuidGenerator.guid();
+		}
 
 		// MARK: PUBLIC: 
 
@@ -55,15 +76,23 @@ namespace Arcy.Quests
 
 		public void FinishQuest()
 		{
-
-		}
-
-		public void ReceiveRewards()
-		{
-
 		}
 
 		// MARK: PRIVATE:
+
+		// private int[] requiredInts()
+		// {
+		// 	List<int> requirements = new List<int>();
+
+		// 	if (requiredPlayerLvl != 0) requirements.Add(requiredPlayerLvl);
+		// 	if (requiredPreviousQuestID != 0) requirements.Add(requiredPreviousQuestID);
+		// 	if (requiredItemID != 0) requirements.Add(requiredItemID);
+		// 	if (requiredPreviousDialogueID != 0) requirements.Add(requiredPreviousDialogueID);
+		// 	if (initialDialogueID != 0) requirements.Add(initialDialogueID);
+		// 	if (requiredBattleID != 0) requirements.Add(requiredBattleID);
+
+		// 	return requirements.ToArray();
+		// }
 
 	}
 }
